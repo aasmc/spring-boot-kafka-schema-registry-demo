69ffb0f13863f94c7448a3e79a4b3272249dad7b
aleksas <aasmc@yandex.ru>
2023-11-08 11:56:10 +0300

    feat: add KafkaHeaders.KEY header for partitioning

--- reviewproducer/src/main/kotlin/ru/aasmc/reviewproducer/service/KafkaReviewServiceImpl.kt
+++ reviewproducer/src/main/kotlin/ru/aasmc/reviewproducer/service/KafkaReviewServiceImpl.kt
@@ -15,28 +15,30 @@ const val X_USER_ID_HEADER = "X-USER-ID"
 const val REVIEW_PLACED_BINDING = "reviewSupplier-out-0"
 const val X_SERVICE_ORIGIN_HEADER = "X-SERVICE-ORIGIN"
 const val X_PARTITION_KEY_HEADER = "X-PARTITION-KEY"
+
 @Service
 class KafkaReviewServiceImpl(
-    private val streamBridge: StreamBridge,
-    private val serviceUtil: ServiceUtil
+        private val streamBridge: StreamBridge,
+        private val serviceUtil: ServiceUtil
 ) : KafkaReviewService {
     override fun sendReviewPlacedEvent(review: ProductReview, userId: UUID) {
         val event = ReviewPlacedEvent.newBuilder()
-            .setReviewId(review.reviewId.toString())
-            .setBody(review.body)
-            .setRating(review.rating)
-            .setEventId(UUID.randomUUID().toString())
-            .build()
+                .setReviewId(review.reviewId.toString())
+                .setBody(review.body)
+                .setRating(review.rating)
+                .setEventId(UUID.randomUUID().toString())
+                .build()
         sendMessage(REVIEW_PLACED_BINDING, event, userId)
     }
 
     private fun sendMessage(bindingName: String, event: ReviewPlacedEvent, userId: UUID) {
         log.debug("Sending a {} message to {}", event, bindingName)
         val message = MessageBuilder.withPayload(event)
-            .setHeader(X_PARTITION_KEY_HEADER, event.reviewId)
-            .setHeader(X_USER_ID_HEADER, userId)
-            .setHeader(X_SERVICE_ORIGIN_HEADER, serviceUtil.getServiceAddress())
-            .build()
+                .setHeader(KafkaHeaders.KEY, event.reviewId)
+                .setHeader(X_PARTITION_KEY_HEADER, event.reviewId)
+                .setHeader(X_USER_ID_HEADER, userId)
+                .setHeader(X_SERVICE_ORIGIN_HEADER, serviceUtil.getServiceAddress())
+                .build()
         streamBridge.send(bindingName, message)
     }
 }
