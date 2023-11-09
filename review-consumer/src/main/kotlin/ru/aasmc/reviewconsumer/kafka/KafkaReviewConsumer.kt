40615e8dde631b98c092ee4b1af56f22c846e3f6
Alex Alexeyev <aasmc@yandex.ru>
2023-11-09 18:33:32 +0300

    feat: remove avro schema from consumer and make it use GenericRecord instead

--- review-consumer/src/main/kotlin/ru/aasmc/reviewconsumer/kafka/KafkaReviewConsumer.kt
+++ review-consumer/src/main/kotlin/ru/aasmc/reviewconsumer/kafka/KafkaReviewConsumer.kt
@@ -1,10 +1,11 @@
 package ru.aasmc.reviewconsumer.kafka
 
+import org.apache.avro.generic.GenericRecord
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
-import ru.aasmc.review.ReviewPlacedEvent
+import java.util.UUID
 import java.util.function.Consumer
 
 private val log = LoggerFactory.getLogger(KafkaReviewConsumer::class.java)
@@ -14,13 +15,18 @@ const val X_SERVICE_ORIGIN_HEADER = "X-SERVICE-ORIGIN"
 class KafkaReviewConsumer {
 
     @Bean
-    fun consumeReviewPlacedEvent() = Consumer { input: Message<ReviewPlacedEvent> ->
+    fun consumeReviewPlacedEvent() = Consumer { input: Message<GenericRecord> ->
         log.info(
             "Received ReviewPlacedEvent {}. Service origin: {}. User id = {}",
             input.payload,
             input.headers[X_SERVICE_ORIGIN_HEADER],
             input.headers[X_USER_ID_HEADER]
         )
+        val record = input.payload
+        val reviewId = UUID.fromString(record["reviewId"].toString())
+        val body = record["body"].toString()
+        val rating = record["rating"] as Int
+        log.info("ReviewPlacedEvent values. ReviewId = {}, Body = {}, Rating = {}", reviewId, body, rating)
     }
 
 }
